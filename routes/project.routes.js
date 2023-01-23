b9574c72907f75f8bf7613620d04d69316203fba
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-01-23 13:39:43 +0100

    add models + route to create projects + route to create tasks

---
+++ routes/project.routes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const mongoose = require('mongoose');
+
+const Project = require('../models/Project.model');
+const Task = require('../models/Task.model');
+
+
+//  POST /api/projects  -  Creates a new project
+router.post('/projects', (req, res, next) => {
+  const { title, description } = req.body;
+
+  Project.create({ title, description, tasks: [] })
+    .then(response => res.json(response))
+    .catch(err => {
+        console.log("error creating new project", err);
+        res.status(500).json(err)
+    });
+});
+
+
+
+module.exports = router;
